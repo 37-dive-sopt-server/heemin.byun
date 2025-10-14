ca6a3b4ca8219463227b6d56fe90a535955bbe1e
byunhm02 <byunhm02@gmail.com>
2025-10-14 18:23:28 +0900

    feat: Member,Gender도메인 설정

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -4,10 +4,18 @@ public class Member {
 
     private Long id;
     private String name;
+    private String birthdate;
+    private String email;
+    private Gender gender;
+    private boolean isDeleted;
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name, String birthdate, String email, Gender gender) {
         this.id = id;
         this.name = name;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.gender = gender;
+        this.isDeleted = false;
     }
 
     public Long getId() {
@@ -16,5 +24,24 @@ public class Member {
 
     public String getName() {
         return name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 }
