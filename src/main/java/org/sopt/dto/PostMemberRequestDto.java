c562f43427f261057cb627807dcd0e2d25eb527a
byunhm02 <byunhm02@gmail.com>
2025-10-28 19:38:47 +0900

    fix:#4 validation추가

--- src/main/java/org/sopt/dto/PostMemberRequestDto.java
+++ src/main/java/org/sopt/dto/PostMemberRequestDto.java
@@ -1,14 +1,23 @@
 package org.sopt.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.sopt.domain.Gender;
 import lombok.Getter;
 
 @Getter
 public class PostMemberRequestDto {
 
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @NotBlank(message = "생년월일은 필수입니다.")
     private String birthdate;
+
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
+
+    @NotNull(message = "성별은 필수입니다.")
     private Gender gender;
 
 }
