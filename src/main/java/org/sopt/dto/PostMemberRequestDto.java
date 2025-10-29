28d92364877b1cdd17de039223f0dd9ee20f61c8
byunhm02 <byunhm02@gmail.com>
2025-10-29 19:47:40 +0900

    feat:#4 Validation 검증 추가

--- src/main/java/org/sopt/dto/PostMemberRequestDto.java
+++ src/main/java/org/sopt/dto/PostMemberRequestDto.java
@@ -1,7 +1,9 @@
 package org.sopt.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.sopt.domain.Gender;
 
 
@@ -11,9 +13,12 @@ public record PostMemberRequestDto(
         String name,
 
         @NotBlank(message = "생년월일은 필수입니다.")
+        @Pattern(regexp = "\\d{4}\\.\\d{2}\\.\\d{2}",
+                message = "생년월일 형식은 yyyy.MM.dd 입니다.")
         String birthdate,
 
         @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
         @NotNull(message = "성별은 필수입니다.")
