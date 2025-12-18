9c4a67a8b77689553a0ffbd118efc5090bee837d
byunhm02 <byunhm02@gmail.com>
2025-12-18 12:47:04 +0900

    feat:#10 스웨거 설정 추가

---
+++ src/main/java/org/sopt/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package org.sopt.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SOPT API")
+                        .description("SOPT 과제 API 문서")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("byunheemin")
+                                .email("byunhm02@gmail.com")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("로컬 서버")
+                ));
+    }
+}
